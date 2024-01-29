098e1a2240d43e635342bdd11bd43b02be7c0b8c
Alexandra <alexandradepourtoux93@gmai.com>
2024-01-29 13:22:35 +0100

    test

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -1,4 +1,3 @@
-/* eslint-disable camelcase */
 // Import access to database tables
 const tables = require("../tables");
 
@@ -6,10 +5,10 @@ const tables = require("../tables");
 const browse = async (req, res, next) => {
   try {
     // Fetch all items from the database
-    const books = await tables.book.readAll();
+    const users = await tables.user.readAll();
 
     // Respond with the items in JSON format
-    res.json(books);
+    res.json(users);
   } catch (err) {
     // Pass any errors to the error-handling middleware
     next(err);
@@ -20,14 +19,14 @@ const browse = async (req, res, next) => {
 const read = async (req, res, next) => {
   try {
     // Fetch a specific item from the database based on the provided ID
-    const books = await tables.book.read(req.params.id);
+    const users = await tables.user.read(req.params.id);
 
     // If the item is not found, respond with HTTP 404 (Not Found)
     // Otherwise, respond with the item in JSON format
-    if (books == null) {
+    if (users == null) {
       res.sendStatus(404);
     } else {
-      res.json(books);
+      res.json(users);
     }
   } catch (err) {
     // Pass any errors to the error-handling middleware
@@ -38,36 +37,20 @@ const read = async (req, res, next) => {
 // The E of BREAD - Edit (Update) operation
 // This operation is not yet implemented
 const edit = async (req, res, next) => {
-  const {
-    image,
-    titre,
-    auteur,
-    nombre_pages,
-    date,
-    categorie,
-    description,
-    commentaire,
-    lu,
-  } = req.body;
-  const updatedBooks = {
+  const { pseudo, email, avatar } = req.body;
+  const updatedUsers = {
     id: req.params.id,
-    image,
-    titre,
-    auteur,
-    nombre_pages,
-    date,
-    categorie,
-    description,
-    commentaire,
-    lu,
+    pseudo,
+    email,
+    avatar,
   };
   try {
-    const existingBooks = await tables.book.read(req.params.id);
-    if (existingBooks == null) {
-      res.status(404).send("Books not found");
+    const existingUsers = await tables.user.read(req.params.id);
+    if (existingUsers == null) {
+      res.status(404).send("Users not found");
     } else {
-      const result = await tables.user.update(updatedBooks);
-      result.status(200).json(updatedBooks);
+      const result = await tables.user.update(updatedUsers);
+      result.status(200).json(updatedUsers);
     }
   } catch (err) {
     next(err);
@@ -77,11 +60,11 @@ const edit = async (req, res, next) => {
 // The A of BREAD - Add (Create) operation
 const add = async (req, res, next) => {
   // Extract the item data from the request body
-  const books = req.body;
+  const users = req.body;
 
   try {
     // Insert the item into the database
-    const insertId = await tables.book.create(books);
+    const insertId = await tables.user.create(users);
 
     // Respond with HTTP 201 (Created) and the ID of the newly inserted item
     res.status(201).json({ insertId });
@@ -95,7 +78,7 @@ const destroy = async (req, res, next) => {
   // Extract the item data from the request body
   try {
     // Insert the item into the database
-    const result = await tables.book.delete(req.params.id);
+    const result = await tables.user.delete(req.params.id);
 
     // Respond with HTTP 201 (Created) and the ID of the newly inserted item
     res.status(201).send(result);
